c1dfa6ce10dc6df0e98672f2ea866e2e6e8dc8ae
bxstar <yurongsheng@163.com>
2014-04-15 19:32:16 +0800

    Campaign报表增加多个Conversion，图表增加Channel选项

--- WriteCampaignReport.vb
+++ WriteCampaignReport.vb
@@ -12,15 +12,15 @@ Sub WriteCampaignReport()
     '定义日期数组
     Dim arrDate As Variant
     '定义多维数组，存放对比的两个月的汇总数据
-    Dim twoMonthDataArr(1 To 12, 1 To 2)
+    Dim twoMonthDataArr(1 To 28, 1 To 2)
     '定义多维数组，存放对比的两周的汇总数据
-    Dim twoWeekDataArr(1 To 12, 1 To 2)
+    Dim twoWeekDataArr(1 To 28, 1 To 2)
     '定义多维数组，存放对比的两天的数据
-    Dim twoDayDataArr(1 To 12, 1 To 2)
+    Dim twoDayDataArr(1 To 28, 1 To 2)
     '月份数据，数据中最小的两个月
     Dim twoMonthArr(1 To 2) As Variant, dicMonth As Variant, arrMonth As Variant
     '指标数据
-    Dim fImpression, fClick, fCost, fConversion, fRevenue
+    Dim fImpression, fClick, fCost, fConversion, fConversion1, fConversion2, fConversion3, fConversion4, fRevenue
     '1元人民币对应的外币
     Dim rate As Double, currencyFormat As Variant
     
@@ -70,6 +70,12 @@ Sub WriteCampaignReport()
     Sheets(reportName).Shapes("ddlCampaignChannel").ControlFormat.ListIndex = 1
     Sheets(reportName).Shapes("ddlCampaignChannel").ControlFormat.AddItem arrChannels
     Sheets(reportName).Shapes("ddlCampaignChannel").OnAction = "DDLChannelChanged"
+    
+    Sheets(reportName).Shapes("ddlCampaignChartChannel").ControlFormat.List = "all"
+    Sheets(reportName).Shapes("ddlCampaignChartChannel").ControlFormat.ListIndex = 1
+    Sheets(reportName).Shapes("ddlCampaignChartChannel").ControlFormat.AddItem arrChannels
+    Sheets(reportName).Shapes("ddlCampaignChartChannel").OnAction = "CampaignChartDataTypeChange"
+    
     'Campaign下拉列表清空，再赋值
     arrCampaigns = DDLSourceFromDataColumn("C", dataSheetName, False, 3)
     Sheets(reportName).Shapes("ddlCampaign").ControlFormat.List = "all"
@@ -126,7 +132,11 @@ Sub WriteCampaignReport()
         fImpression = Sheets(dataSheetName).Cells(rowIndex, 4)
         fClick = Sheets(dataSheetName).Cells(rowIndex, 5)
         fCost = Sheets(dataSheetName).Cells(rowIndex, 6) * rate
-        fConversion = Sheets(dataSheetName).Cells(rowIndex, 7)
+        fConversion1 = Sheets(dataSheetName).Cells(rowIndex, 7)
+        fConversion2 = Sheets(dataSheetName).Cells(rowIndex, 8)
+        fConversion3 = Sheets(dataSheetName).Cells(rowIndex, 9)
+        fConversion4 = Sheets(dataSheetName).Cells(rowIndex, 10)
+        fConversion = fConversion1 + fConversion2 + fConversion3 + fConversion4
         fRevenue = Sheets(dataSheetName).Cells(rowIndex, 11) * rate
 
         If (channelName = strChannel Or channelName = "" Or channelName = "all") And (campaignName = strCampaign Or campaignName = "" Or campaignName = "all") Then
@@ -139,22 +149,22 @@ Sub WriteCampaignReport()
             '两月对比数据
             strYearMonth = CStr(Year(strDate)) & "/" & CStr(Month(strDate))
             If strYearMonth = twoMonthArr(1) Then
-                Call FillArrayData(twoMonthDataArr, 1, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoMonthDataArr, 1, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
             If strYearMonth = twoMonthArr(2) Then
-                Call FillArrayData(twoMonthDataArr, 2, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoMonthDataArr, 2, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
 
             '两周对比数据
             If IsInWeek(CStr(arrDate(1, 1)), strDate) Then
-                Call FillArrayData(twoWeekDataArr, 1, fImpression, fClick, fConversion, fCost, fRevenue)
-                Call FillArrayData(twoWeekDataArr, 2, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoWeekDataArr, 1, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoWeekDataArr, 2, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
 
             '两天对比数据
             If CStr(arrDate(1, 1)) = strDate Then
-                Call FillArrayData(twoDayDataArr, 1, fImpression, fClick, fConversion, fCost, fRevenue)
-                Call FillArrayData(twoDayDataArr, 2, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoDayDataArr, 1, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoDayDataArr, 2, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
 
         End If
@@ -167,16 +177,16 @@ Sub WriteCampaignReport()
 
     '向报表中写入前后对比的两周数据
     Call ComMetricArrayData(twoWeekDataArr)
-    Call FillCellData(reportName, 24, 4, twoWeekDataArr)
+    Call FillCellData(reportName, 40, 4, twoWeekDataArr)
 
     '向报表中写入前后对比的两天数据
     Call ComMetricArrayData(twoDayDataArr)
-    Call FillCellData(reportName, 24, 9, twoDayDataArr)
+    Call FillCellData(reportName, 40, 9, twoDayDataArr)
     
     '设置单个格货币格式
     Call SetCurrencyFormat(reportName, 9, 4, currencyFormat)
-    Call SetCurrencyFormat(reportName, 24, 4, currencyFormat)
-    Call SetCurrencyFormat(reportName, 24, 9, currencyFormat)
+    Call SetCurrencyFormat(reportName, 40, 4, currencyFormat)
+    Call SetCurrencyFormat(reportName, 40, 9, currencyFormat)
 
     '绘制饼图
     Call ChartForCampaign
@@ -184,7 +194,7 @@ End Sub
 
 Sub DDLCampaignChannelChanged()
         
-    Dim fImpression, fClick, fCost, fConversion, fRevenue
+    Dim fImpression, fClick, fCost, fConversion, fConversion1, fConversion2, fConversion3, fConversion4, fRevenue
     Dim strChannel As String, strDate As String, strCampaign As String, strYearMonth As String
     Dim channelName As String, campaignName As String, dataSheetName As String, reportName As String
     Dim monthStart As String, monthEnd As String
@@ -193,11 +203,11 @@ Sub DDLCampaignChannelChanged()
     '是否符合campaign,channel条件的数据
     Dim filterFlag As Boolean
     '定义多维数组，存放对比的两个月的汇总数据
-    Dim twoMonthDataArr(1 To 12, 1 To 2)
+    Dim twoMonthDataArr(1 To 28, 1 To 2)
     '定义多维数组，存放对比的两周的汇总数据
-    Dim twoWeekDataArr(1 To 12, 1 To 2)
+    Dim twoWeekDataArr(1 To 28, 1 To 2)
     '定义多维数组，存放对比的两天的数据
-    Dim twoDayDataArr(1 To 12, 1 To 2)
+    Dim twoDayDataArr(1 To 28, 1 To 2)
     '1元人民币对应的外币
     Dim rate As Double
     
@@ -228,7 +238,11 @@ Sub DDLCampaignChannelChanged()
         fImpression = Sheets(dataSheetName).Cells(rowIndex, 4)
         fClick = Sheets(dataSheetName).Cells(rowIndex, 5)
         fCost = Sheets(dataSheetName).Cells(rowIndex, 6) * rate
-        fConversion = Sheets(dataSheetName).Cells(rowIndex, 7)
+        fConversion1 = Sheets(dataSheetName).Cells(rowIndex, 7)
+        fConversion2 = Sheets(dataSheetName).Cells(rowIndex, 8)
+        fConversion3 = Sheets(dataSheetName).Cells(rowIndex, 9)
+        fConversion4 = Sheets(dataSheetName).Cells(rowIndex, 10)
+        fConversion = fConversion1 + fConversion2 + fConversion3 + fConversion4
         fRevenue = Sheets(dataSheetName).Cells(rowIndex, 11) * rate
     
         If (channelName = strChannel Or channelName = "" Or channelName = "all") And (campaignName = strCampaign Or campaignName = "" Or campaignName = "all") Then
@@ -242,27 +256,27 @@ Sub DDLCampaignChannelChanged()
             '两月对比数据
             strYearMonth = CStr(Year(strDate)) & "/" & CStr(Month(strDate))
             If strYearMonth = monthStart Then
-                Call FillArrayData(twoMonthDataArr, 1, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoMonthDataArr, 1, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
 
             If strYearMonth = monthEnd Then
-                Call FillArrayData(twoMonthDataArr, 2, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoMonthDataArr, 2, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
             
             '两周对比数据
             If IsInWeek(weekStart, strDate) Then
-                Call FillArrayData(twoWeekDataArr, 1, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoWeekDataArr, 1, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
             If IsInWeek(weekEnd, strDate) Then
-                Call FillArrayData(twoWeekDataArr, 2, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoWeekDataArr, 2, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
             
             '两天对比数据
             If dayStart = strDate Then
-                Call FillArrayData(twoDayDataArr, 1, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoDayDataArr, 1, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
             If dayEnd = strDate Then
-                Call FillArrayData(twoDayDataArr, 2, fImpression, fClick, fConversion, fCost, fRevenue)
+                Call FillArrayData(twoDayDataArr, 2, fImpression, fClick, fConversion1, fConversion2, fConversion3, fConversion4, fConversion, fCost, fRevenue)
             End If
         End If
         
@@ -275,11 +289,11 @@ Sub DDLCampaignChannelChanged()
 
     '向报表中写入前后对比的两周数据
     Call ComMetricArrayData(twoWeekDataArr)
-    Call FillCellData(reportName, 24, 4, twoWeekDataArr)
+    Call FillCellData(reportName, 40, 4, twoWeekDataArr)
 
     '向报表中写入前后对比的两天数据
     Call ComMetricArrayData(twoDayDataArr)
-    Call FillCellData(reportName, 24, 9, twoDayDataArr)
+    Call FillCellData(reportName, 40, 9, twoDayDataArr)
     
 End Sub
 
@@ -313,18 +327,18 @@ Sub ChartForCampaign()
     Set dicCampaign = CreateObject("Scripting.Dictionary")
     '源表中得到有数据的行
     n = Sheets(dataSheetName).[A65536].End(xlUp).Row
-    arr = Sheets(dataSheetName).Range("C3:K" & n)
+    arr = Sheets(dataSheetName).Range("A3:K" & n)
     
     For i = 1 To UBound(arr)
-        If dicCampaign(arr(i, 1)) = "" Then
-            dicCampaign(arr(i, 1)) = i
+        If dicCampaign(arr(i, 3)) = "" Then
+            dicCampaign(arr(i, 3)) = i
             rowCount = rowCount + 1
         End If
-        dicMetricData(arr(i, 1) & "Imp") = dicMetricData(arr(i, 1) & "Imp") + arr(i, 2)
-        dicMetricData(arr(i, 1) & "Click") = dicMetricData(arr(i, 1) & "Click") + arr(i, 3)
-        dicMetricData(arr(i, 1) & "Cost") = dicMetricData(arr(i, 1) & "Cost") + arr(i, 4) * rate
-        dicMetricData(arr(i, 1) & "Conversion") = dicMetricData(arr(i, 1) & "Conversion") + arr(i, 5)
-        dicMetricData(arr(i, 1) & "Revenue") = dicMetricData(arr(i, 1) & "Revenue") + arr(i, 9) * rate
+        dicMetricData(arr(i, 3) & "Imp") = dicMetricData(arr(i, 3) & "Imp") + arr(i, 4)
+        dicMetricData(arr(i, 3) & "Click") = dicMetricData(arr(i, 3) & "Click") + arr(i, 5)
+        dicMetricData(arr(i, 3) & "Cost") = dicMetricData(arr(i, 3) & "Cost") + arr(i, 6) * rate
+        dicMetricData(arr(i, 3) & "Conversion") = dicMetricData(arr(i, 3) & "Conversion") + arr(i, 7) + arr(i, 8) + arr(i, 9) + arr(i, 10)
+        dicMetricData(arr(i, 3) & "Revenue") = dicMetricData(arr(i, 3) & "Revenue") + arr(i, 11) * rate
     Next
     
     Erase arr
@@ -352,7 +366,7 @@ Sub ChartForCampaign()
     
     '没有饼图重新创建，有的话使用老格式
     If Not IsShapeExists(reportName, "campaignChart") Then
-        Call Make3DPieChart(reportName, "AA2:AA" & (rowCount + 1) & ",AP2:AP" & (rowCount + 1), reportName, Range("H5").left, Range("H5").top, 450, 265, "campaignChart", "All Campaign", True)
+        Call Make3DPieChart(reportName, "AA2:AA" & (rowCount + 1) & ",AP2:AP" & (rowCount + 1), reportName, Range("H5").left, Range("H5").top, 500, 250, "campaignChart", "All Campaign", True)
     Else
         Sheets(reportName).ChartObjects("campaignChart").Chart.SetSourceData (Sheets(reportName).Range("AA2:AA" & (rowCount + 1) & ",AP2:AP" & (rowCount + 1)))
     End If
@@ -368,7 +382,114 @@ Sub ChartForCampaign()
     End If
     
     Sheets(reportName).Shapes("chartMetric1").ZOrder msoBringToFront
+    Sheets(reportName).Shapes("ddlCampaignChartChannel").ZOrder msoBringToFront
     ActiveSheet.Range("I8").Select
+End Sub
+
+Sub CampaignChartDataTypeChange()
+'图形选项Channel被选择
+
+    Dim metricFields() As Variant
+    Dim i As Long, n As Long, j As Long, rowCount As Integer
+    Dim dicMetricData, dicDate, arr, arrKeys
+    Dim dataSheetName As String, reportName As String, channelName As String, cellChannel As String
+    Dim cellDate As Variant, cellYearMonth As String
+    Dim rate As Double
+    Dim filterFlag As Boolean
+    
+    Application.ScreenUpdating = False
+    dataSheetName = "Campaign元数据"
+    reportName = "Campaign"
+    
+    '汇率
+    rate = Sheets(dataSheetName).Cells(1, 4)
+    
+    '得到有数据的行，需要清理
+    n = Sheets(reportName).[AA65536].End(xlUp).Row
+    If n > 1 Then
+        Sheets(reportName).Range("AA2:AQ" & n).ClearContents
+    End If
+    
+    channelName = ValueDDL("ddlCampaignChartChannel", reportName)
+    
+    '指标数据字典
+    Set dicMetricData = CreateObject("Scripting.Dictionary")
+    '计划名称字典
+    Set dicCampaign = CreateObject("Scripting.Dictionary")
+    '源表中得到有数据的行
+    n = Sheets(dataSheetName).[A65536].End(xlUp).Row
+    arr = Sheets(dataSheetName).Range("A3:K" & n)
+    
+    For i = 1 To UBound(arr)
+        '取得数据中的渠道
+        cellChannel = arr(i, 1)
+        
+        If (channelName = cellChannel Or channelName = "all") Then
+            filterFlag = True
+        Else
+            filterFlag = False
+        End If
+        
+        If filterFlag Then
+        
+            If dicCampaign(arr(i, 3)) = "" Then
+                dicCampaign(arr(i, 3)) = i
+                rowCount = rowCount + 1
+            End If
+            dicMetricData(arr(i, 3) & "Imp") = dicMetricData(arr(i, 3) & "Imp") + arr(i, 4)
+            dicMetricData(arr(i, 3) & "Click") = dicMetricData(arr(i, 3) & "Click") + arr(i, 5)
+            dicMetricData(arr(i, 3) & "Cost") = dicMetricData(arr(i, 3) & "Cost") + arr(i, 6) * rate
+            dicMetricData(arr(i, 3) & "Conversion") = dicMetricData(arr(i, 3) & "Conversion") + arr(i, 7) + arr(i, 8) + arr(i, 9) + arr(i, 10)
+            dicMetricData(arr(i, 3) & "Revenue") = dicMetricData(arr(i, 3) & "Revenue") + arr(i, 11) * rate
+            
+        End If
+    Next
+    
+    Erase arr
+    arr = Sheets(reportName).Range("AA2", Sheets(reportName).Cells(rowCount + 1, 32))
+    
+    arrKeys = dicCampaign.keys
+    
+    For i = 1 To rowCount
+        arr(i, 1) = arrKeys(i - 1)
+        arr(i, 2) = dicMetricData(arr(i, 1) & "Imp")
+        arr(i, 3) = dicMetricData(arr(i, 1) & "Click")
+        arr(i, 4) = dicMetricData(arr(i, 1) & "Cost")
+        arr(i, 5) = dicMetricData(arr(i, 1) & "Conversion")
+        arr(i, 6) = dicMetricData(arr(i, 1) & "Revenue")
+    Next
+    
+    If rowCount = 0 Then Exit Sub
+    
+    Sheets(reportName).[AA2].Resize(rowCount, UBound(arr, 2)) = arr
+    Erase arr
+    Set dicCampaign = Nothing
+    Set dicMetricData = Nothing
+    
+    '图形的数据源，一列
+    Call CopyPasteWithDesign(reportName, "AB2:AB" & rowCount + 1, reportName, "ap2")
+    
+    '没有饼图重新创建，有的话使用老格式
+    If Not IsShapeExists(reportName, "campaignChart") Then
+        Call Make3DPieChart(reportName, "AA2:AA" & (rowCount + 1) & ",AP2:AP" & (rowCount + 1), reportName, Range("H5").left, Range("H5").top, 500, 250, "campaignChart", "All Campaign", True)
+    Else
+        Sheets(reportName).ChartObjects("campaignChart").Chart.SetSourceData (Sheets(reportName).Range("AA2:AA" & (rowCount + 1) & ",AP2:AP" & (rowCount + 1)))
+    End If
+    
+    '设置在图表中显示隐藏数据
+    Sheets(reportName).ChartObjects("campaignChart").Chart.PlotVisibleOnly = False
+    
+    If Not IsShapeExists(reportName, "chartMetric1") Then
+        metricFields = Array("Imp", "Clicks", "Cost", "Conversion", "Revenue")
+        Call AddDdlInOneCell(reportName, "H5", "CampaignChartMetricChange", metricFields, "chartMetric1", 1)
+    Else
+        Sheets(reportName).Shapes("chartMetric1").ControlFormat.ListIndex = 1
+    End If
+    
+    Sheets(reportName).Shapes("chartMetric1").ZOrder msoBringToFront
+    Sheets(reportName).Shapes("ddlCampaignChartChannel").ZOrder msoBringToFront
+    ActiveSheet.Range("I8").Select
+
 End Sub
 
 Sub CampaignChartMetricChange()
